e414e87b513cc793674a63f7d841a991613cffbc
isaachowen <31664870+isaachowen@users.noreply.github.com>
2020-02-05 11:53:41 -0500

    Update Analysis_1_thru_20.java

--- DiscoDNC Tandem Network/Analysis_1_thru_20.java
+++ DiscoDNC Tandem Network/Analysis_1_thru_20.java
@@ -75,6 +75,7 @@ public class Analysis_1_thru_20 {
     public void run() throws Exception {
     	org.networkcalculus.num.Num[] res_tfa = new org.networkcalculus.num.Num[20];
     	org.networkcalculus.num.Num[] res_sfa = new org.networkcalculus.num.Num[20];
+    	org.networkcalculus.num.Num[] res_pmoo = new org.networkcalculus.num.Num[20];
     	
 		for(int i=1;i<=20;i++) {
 			Tandem_ServerGraph_Generator network_factory = new Tandem_ServerGraph_Generator(i);
@@ -90,19 +91,30 @@ public class Analysis_1_thru_20 {
 			}
 			
 	        Set<ArrivalBoundMethod> arrival_bound_methods = new HashSet<ArrivalBoundMethod>(Collections.singleton(ArrivalBoundMethod.AGGR_PBOO_CONCATENATION)); 
-	        AnalysisConfig configuration = 
+	        AnalysisConfig configuration_FIFO = 
+	        		new AnalysisConfig( MultiplexingEnforcement.GLOBAL_FIFO, MaxScEnforcement.GLOBALLY_OFF, MaxScEnforcement.SERVER_LOCAL, 
+	        				arrival_bound_methods, 
+	        				false, false,false);
+	        
+	        AnalysisConfig configuration_ARB = 
 	        		new AnalysisConfig( MultiplexingEnforcement.GLOBAL_ARBITRARY, MaxScEnforcement.GLOBALLY_OFF, MaxScEnforcement.SERVER_LOCAL, 
 	        				arrival_bound_methods, 
-	        				false, false, true);
+	        				false, false, false);
 			
-			TotalFlowAnalysis tfa = new TotalFlowAnalysis(network);
-			SeparateFlowAnalysis sfa = new SeparateFlowAnalysis(network);
+			TotalFlowAnalysis tfa = new TotalFlowAnalysis(network,configuration_ARB);
+			SeparateFlowAnalysis sfa = new SeparateFlowAnalysis(network,configuration_ARB);
+			PmooAnalysis pmoo = new PmooAnalysis(network,configuration_ARB);
+			
 			tfa.performAnalysis(foi);
 			sfa.performAnalysis(foi);
+			pmoo.performAnalysis(foi);
 			org.networkcalculus.num.Num restfa = tfa.getDelayBound();
 			org.networkcalculus.num.Num ressfa = sfa.getDelayBound();
+			org.networkcalculus.num.Num respmoo = pmoo.getDelayBound();
+			
 			res_tfa[i-1] = restfa;
 			res_sfa[i-1] = ressfa;
+			res_pmoo[i-1] = respmoo;
 		}
 		
 		// This prints out the flow of interest latencies for tandem networks of size 1-20 using TFA, and then using SFA
@@ -113,6 +125,10 @@ public class Analysis_1_thru_20 {
 		System.out.println("SFA: ");
 		for (int i = 0; i< 20;i ++) {
 			System.out.println((i+1)+": "+res_sfa[i]);
+		}
+		System.out.println("PMOO: ");
+		for (int i = 0; i< 20;i ++) {
+			System.out.println((i+1)+": "+res_pmoo[i]);
 		}
 		
     }
